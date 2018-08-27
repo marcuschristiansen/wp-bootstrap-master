9e4988636f27987bbb47e10cb6cb3c898bc85b7e
Marcus Christiansen <marcus@firingsquad.co.za>
2018-08-27 11:51:01 +0200

    fix

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -7,54 +7,57 @@
  * @package wp_starter
  */
 
+if( class_exists('acf') ):
 // check if the flexible content field has rows of data
-if( have_rows('content') ):
+    if( have_rows('content') ):
 
-     // loop through the rows of data
-    while ( have_rows('content') ) : the_row();
+        // loop through the rows of data
+        while ( have_rows('content') ) : the_row();
 
-		/* Slider/Carousel */
-        if( get_row_layout() == 'carousel' ):
+            /* Slider/Carousel */
+            if( get_row_layout() == 'carousel' ):
 
-        	get_template_part( 'template-parts/partials/partial', 'carousel' );
+                get_template_part( 'template-parts/partials/partial', 'carousel' );
 
-        /* Jumbotron */
-        elseif( get_row_layout() == 'jumbotron' ): 
+            /* Jumbotron */
+            elseif( get_row_layout() == 'jumbotron' ): 
 
-        	get_template_part( 'template-parts/partials/partial', 'jumbotron' );
+                get_template_part( 'template-parts/partials/partial', 'jumbotron' );
 
-        /* WYSIWYG */
-        elseif( get_row_layout() == 'wysiwyg' ): 
+            /* WYSIWYG */
+            elseif( get_row_layout() == 'wysiwyg' ): 
 
-            get_template_part( 'template-parts/partials/partial', 'wysiwyg' );
+                get_template_part( 'template-parts/partials/partial', 'wysiwyg' );
 
-        /* Tabs */
-        elseif( get_row_layout() == 'tabbed_content' ): 
+            /* Tabs */
+            elseif( get_row_layout() == 'tabbed_content' ): 
 
-            get_template_part( 'template-parts/partials/partial', 'tabs' );
+                get_template_part( 'template-parts/partials/partial', 'tabs' );
 
-        /* Icon Row */
-        elseif( get_row_layout() == 'icon_row' ): 
-    
-            get_template_part( 'template-parts/partials/partial', 'icons' );
+            /* Icon Row */
+            elseif( get_row_layout() == 'icon_row' ): 
+        
+                get_template_part( 'template-parts/partials/partial', 'icons' );
 
-        /* Icon Row */
-        elseif( get_row_layout() == 'slider' ): 
-            
-            get_template_part( 'template-parts/partials/partial', 'slider' );
+            /* Icon Row */
+            elseif( get_row_layout() == 'slider' ): 
+                
+                get_template_part( 'template-parts/partials/partial', 'slider' );
 
-        /* Icon Row */
-        elseif( get_row_layout() == 'logo_carousel' ): 
-            
-            get_template_part( 'template-parts/partials/partial', 'logo_carousel' );
+            /* Icon Row */
+            elseif( get_row_layout() == 'logo_carousel' ): 
+                
+                get_template_part( 'template-parts/partials/partial', 'logo_carousel' );
 
-        endif;
+            endif;
 
-    endwhile;
+        endwhile;
 
-else :
+    else :
 
-    // no layouts found
+        // no layouts found
+
+    endif;
 
 endif;
 
